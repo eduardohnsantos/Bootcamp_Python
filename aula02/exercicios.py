a8f09eade7430346ebb1827c47633115160fdff1
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2026-05-12 21:28:32 -0300

    exercicio 14

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -1,3 +1,5 @@
+import math
+
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
@@ -26,9 +28,9 @@
 
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
-numero = int(input("Insira um numero: "))
-quadrado = numero ** 2
-print(quadrado)
+# numero = int(input("Insira um numero: "))
+# quadrado = numero ** 2
+# print(quadrado)
 
 
 # #### Números de Ponto Flutuante (`float`)
@@ -39,12 +41,24 @@ print(quadrado)
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
+# raio_do_circulo = float(input("Digite o raio: "))
+# area_do_circulo = math.pi * raio_do_circulo ** 2
+# # area_do_circulo_formatada = "{:.2f}".format(area_do_circulo)
+# print(f"{area_do_circulo:.2f}")
+
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"O elemento 1 e o : {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 2 e o : {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 3 e o : {lista_de_dia_mes_ano[2]}")
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
